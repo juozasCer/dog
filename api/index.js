e3ceb8050c82a37686712fc437eeb6b274caba83
juozasCer <cerjuozas@gmail.com>
2024-08-31 00:58:20 +0300

    t

--- api/index.js
+++ api/index.js
@@ -3,13 +3,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
+const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB Atlas
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
