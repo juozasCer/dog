058ee13f2a932ff67d120a66fb3ae937f6c092ff
juozasCer <cerjuozas@gmail.com>
2024-08-30 23:16:04 +0300

    t

--- game-backend/server.js
+++ game-backend/server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config(); // To use environment variables from .env file
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000; // Use PORT from .env or default to 5000
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://dog-blush-six.vercel.app', // Your Vercel URL
+}));
 app.use(express.json());
 
 // Connect to MongoDB Atlas
-mongoose.connect('mongodb+srv://cerjuozas:c3fjrfoqvHkYYxjx@cluster1.lsiee.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1', {
+const mongodbUri = process.env.MONGODB_URI; // Use MongoDB URI from .env
+mongoose.connect(mongodbUri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
