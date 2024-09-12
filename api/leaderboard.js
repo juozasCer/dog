72079aa5306cb2eb8ea0faf91c9ab1e182e2d636
juozasCer <cerjuozas@gmail.com>
2024-09-12 14:48:37 +0300

    t

--- api/leaderboard.js
+++ api/leaderboard.js
@@ -72,3 +72,9 @@ app.get('/api/spawnRate', (req, res) => {
 });
 
 module.exports = app;
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
