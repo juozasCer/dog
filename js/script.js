b3884de4df66538ca720f8d28259dc3c5e2d97cb
juozasCer <cerjuozas@gmail.com>
2024-09-12 14:57:31 +0300

    t

--- js/script.js
+++ js/script.js
@@ -163,8 +163,8 @@ setNameButton.addEventListener('click', () => {
     }
 });
 
-function fetchSpawnRate() {
-    return fetch('/api/leaderboard')
+async function fetchSpawnRate() {
+    return fetch('/api/spawnRate')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
