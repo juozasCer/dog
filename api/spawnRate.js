ea176437cbac6d88fbc55aaa3b2d22c849fc3936
juozasCer <cerjuozas@gmail.com>
2024-09-12 04:43:45 +0300

    t

--- api/spawnRate.js
+++ api/spawnRate.js
@@ -1,5 +1,5 @@
 // GET endpoint to retrieve spawn rate
-app.get('', (req, res) => {
+app.get('api/spawnRate', (req, res) => {
   // Return a fixed spawn rate (e.g., 0.05) for simplicity
   console.log('Fetching spawn rate');
   res.json({ spawnRate: 0.05 });
