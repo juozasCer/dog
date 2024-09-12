47ca9b7e26eccd425ad9010c5082f861802d957a
juozasCer <cerjuozas@gmail.com>
2024-09-12 15:59:18 +0300

    t

--- api/spawnRate.js
+++ api/spawnRate.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/api/spawnRate', (req, res) => {
   console.log('Fetching spawn rate');
-  res.json({ spawnRate: 1 });
+  res.json({ spawnRate: 0.05 });
 });
 
 module.exports = router;
