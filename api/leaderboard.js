5cd2118dea86468b07334d4fd363369161e83136
juozasCer <cerjuozas@gmail.com>
2024-09-12 15:31:10 +0300

    t

--- api/leaderboard.js
+++ api/leaderboard.js
@@ -65,11 +65,11 @@ app.post('/api/leaderboard', async (req, res) => {
 });
 
 // GET endpoint to retrieve spawn rate
-/*app.get('/api/spawnRate', (req, res) => {
+app.get('/api/spawnRate', (req, res) => {
   // Return a fixed spawn rate (e.g., 0.05) for simplicity
   console.log('Fetching spawn rate');
   res.json({ spawnRate: 0.05 });
-});*/
+});
 
 module.exports = app;
 
